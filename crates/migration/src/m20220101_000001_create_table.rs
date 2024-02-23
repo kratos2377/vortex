aed63479960b82dba8e174752ad7803aa9c658c7
kratos2377 <shobityadav23@gmail.com>
2024-02-24 02:47:04 +0530

    Seaorm migration code added

--- crates/migration/src/m20220101_000001_create_table.rs
+++ crates/migration/src/m20220101_000001_create_table.rs
@@ -7,25 +7,105 @@ pub struct Migration;
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
-        todo!();
+        //todo!();
 
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(Users::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Post::Id)
-                            .integer()
+                        ColumnDef::new(User::Id)
+                            .uuid()
                             .not_null()
-                            .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(User::First_Name).string().not_null())
+                    .col(ColumnDef::new(User::Last_Name).string().not_null())
+                    .col(ColumnDef::new(User::Password).string().not_null())
+                    .col(ColumnDef::new(User::Email).string().not_null())
+                    .col(ColumnDef::new(User::Username).string().not_null())
+                    .col(ColumnDef::new(User::Verified).boolean().not_null().default(Value::from(false)))
+                    .col(ColumnDef::new(User::Score).integer().not_null().default(Value::from(0)))
+                    .col(ColumnDef::new(User::CreatedAt).date_time().not_null())
+                    .col(ColumnDef::new(User::UpdatedAt).date_time().not_null())
                     .to_owned(),
             )
-            .await
+            .await;
+
+
+            manager
+            .create_table(
+                Table::create()
+                    .table(UsersFriends::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(UsersFriends::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(UsersFriends::UserId).uuid().not_null())
+                    .col(ColumnDef::new(UsersFriends::FriendId).uuid().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                        .name("fk-user_friend")
+                        .from(UsersFriends::Table, UsersFriends::UserId)
+                        .to(Users::Table, Users::Id),
+                    )
+                    .to_owned(),
+            )
+            .await;
+
+        
+            manager
+            .create_table(
+                Table::create()
+                    .table(UsersWallets::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(UsersWallets::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(UsersWallets::UserId).uuid().not_null())
+                    .col(ColumnDef::new(UsersWallets::WalletAddress).string().not_null())
+                    .col(ColumnDef::new(UsersWallets::WalletType).string().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                        .name("fk-wallet_user_id")
+                        .from(UsersWallets::Table, UsersWallets::UserId)
+                        .to(Users::Table, Users::Id),
+                    )
+                    .to_owned(),
+            )
+            .await;
+
+            manager
+            .create_table(
+                Table::create()
+                    .table(UsersFriendsRequests::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(UsersFriendsRequests::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(UsersFriendsRequests::UserSentId).uuid().not_null())
+                    .col(ColumnDef::new(UsersFriendsRequests::UserRecievedId).uuid().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                        .name("fk-user_sent_id")
+                        .from(UsersFriendsRequests::Table, UsersFriendsRequests::UserSentId)
+                        .to(Users::Table, Users::Id),
+                    )
+                    .to_owned(),
+            )
+            .await;
+
+
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
@@ -33,15 +113,65 @@ impl MigrationTrait for Migration {
         todo!();
 
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
-            .await
+            .drop_table(Table::drop().table(Users::Table).to_owned())
+            .await;
+
+
+            manager
+            .drop_table(Table::drop().table(UsersFriends::Table).to_owned())
+            .await;
+
+        manager
+        .drop_table(Table::drop().table(UsersWallets::Table).to_owned())
+        .await;
+
+        manager
+        .drop_table(Table::drop().table(UsersFriendsRequests::Table).to_owned())
+        .await;
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum Users {
     Table,
     Id,
-    Title,
-    Text,
+    First_Name,
+    Last_Name,
+    Password,
+    Email,
+    Username,
+    Verified,
+    Score,
+    CreatedAt,
+    UpdatedAt,
 }
+
+#[derive(DeriveIden)]
+enum UsersFriends {
+    Table,
+    Id,
+    UserId,
+    FriendId,
+}
+
+#[derive(DeriveIden)]
+enum UsersWallets {
+    Table,
+    Id,
+    UserId,
+    WalletAddress,
+    WalletType,
+}
+
+#[derive(DeriveIden)]
+enum UsersFriendsRequests {
+    Table,
+    Id,
+    UserSentId,
+    UserRecievedId,
+}
+
+
+
+
+
