b7e2c36339b0777625c581c6cd9c101e5dee5e9c
kratos2377 <shobityadav23@gmail.com>
2024-04-21 15:58:09 +0530

    Some minor API method changes

--- crates/messier/src/routes/game_logic_routes.rs
+++ crates/messier/src/routes/game_logic_routes.rs
@@ -14,6 +14,6 @@ pub fn create_game_routes() -> Router<AppDBState> {
         .route("/destroy_lobby_and_game", post(controllers::game_controller::destroy_lobby_and_game))
         .route("/get_ongoing_games_for_user", get(controllers::game_controller::get_ongoing_games_for_user))
         .route("/get_current_state_of_game", get(controllers::game_controller::get_current_state_of_game))
-        .route("/stake_in_game", get(controllers::game_controller::stake_in_game))
+        .route("/stake_in_game", post(controllers::game_controller::stake_in_game))
         .route_layer(middleware::from_fn(utils::middleware::guard))
 }
--- crates/messier/src/routes/user_auth_routes.rs
+++ crates/messier/src/routes/user_auth_routes.rs
@@ -6,7 +6,7 @@ use crate::{controllers, state::AppDBState};
 
 pub fn create_user_routes() -> Router<AppDBState> {
    Router::new()
-        .route("/verigy_token", post(controllers::user_auth_controller::verify_token))
+        .route("/verify_token", post(controllers::user_auth_controller::verify_token))
         .route("/login", post(controllers::user_auth_controller::login_user))
         .route("/registration", post(controllers::user_auth_controller::register_user))
         .route("/send_email", post(controllers::user_auth_controller::send_email))
